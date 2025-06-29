389fd33fc12151e8c1fefa6a503e870d25b84f8d
santiagompaz <122167806+santiagompaz@users.noreply.github.com>
2025-06-29 00:34:02 -0300

    Creación de ProductForm

---
+++ src/components/ProductForm.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+export const ProductForm = ({ onAdd }) => {
+  const [products, setProducts] = useState({
+    name: "",
+    description: "",
+    price: "",
+  });
+
+  const [error, setError] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setProducts({ ...products, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    onAdd(products);
+
+    setProducts({
+      name: "",
+      description: "",
+      price: "",
+    });
+  };
+
+  return <div>ProductForm</div>;
+};
