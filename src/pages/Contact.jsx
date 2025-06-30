503ee2a227bb7835917150104998e45cb8907416
santiagompaz <122167806+santiagompaz@users.noreply.github.com>
2025-06-30 20:57:04 -0300

    Armado de sección Contacto

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
-import { Header } from '../components/Header'
-import { Footer } from '../components/Footer'
-import { Nav } from '../components/Nav'
+import React from "react";
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+import { Nav } from "../components/Nav";
 
 export const Contact = () => {
   return (
     <div>
-        <Header />
-        <h1>Contact</h1>
-        <Footer/>
+      <Header />
+      <h1 style={{margin:"2rem"}}>Formulario de contacto</h1>
+
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">
+          Ingrese su e-mail
+        </label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleFormControlInput1"
+          placeholder="usuario@dominio.com"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">
+          Ingrese su consulta
+        </label>
+        <textarea
+          class="form-control"
+          id="exampleFormControlTextarea1"
+          rows="3"
+          placeholder="Escriba su consulta..."
+        ></textarea>
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
