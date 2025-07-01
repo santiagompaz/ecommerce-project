5bfef3c5bbd57db3db6a7be9767b1e9e69b1f35e
santiagompaz <122167806+santiagompaz@users.noreply.github.com>
2025-07-01 19:25:11 -0300

    Ajustes de estilos en Products

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,34 +1,40 @@
 import React, { useContext } from "react";
-import { useParams } from "react-router-dom";
-import './ProductDetailsStyles.css'
+import { useParams, useNavigate } from "react-router-dom";
+import './ProductDetailsStyles.css';
 import { CartContext } from "../context/CartContext";
 
 export const ProductDetails = () => {
-
   const { products } = useContext(CartContext);
-
   const { id, idUser } = useParams();
+  const navigate = useNavigate();
 
   const product = products.find((prod) => prod.id == id);
 
   return (
     <div>
       {product ? (
-        <div className="product-detail">
-          <div className="image-container-detail">
-            <img
-              src={product.image || placeholder}
-              alt="Imagen del producto"
-              className="image-detail"
-            ></img>
+        <>
+          <div className="product-detail">
+            <div className="image-container-detail">
+              <img
+                src={product.image || placeholder}
+                alt="Imagen del producto"
+                className="image-detail"
+              />
+            </div>
+            <p className="name-detail">{product.name}</p>
+            <p className="description-detail">{product.description}</p>
+            <div className="info-detail">
+              <p className="price-detail">${product.price}</p>
+              <p className="stock-detail">{product.stock} unidades</p>
+            </div>
           </div>
-          <p className="name-detail">{product.name}</p>
-          <p className="description-detail">{product.description}</p>
-          <div className="info-detail">
-            <p className="price-detail">${product.price}</p>
-            <p className="stock-detail">{product.stock} unidades</p>
+          <div className="button-area">
+            <button className="button-primary" onClick={() => navigate(-1)}>
+              Volver
+            </button>
           </div>
-        </div>
+        </>
       ) : (
         <p>Producto no encontrado</p>
       )}
